5a3469fee6fee1de746512444226fc6a7dc9d41d
Chun Hin D. Tang <dtang955472@gmail.com>
2021-05-17 16:24:11 -0400

    update

--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  // destructure the dogs from  props
+  const {dogs} = props
+  
+  // Returns the JSX for when you have dogs
+  const loaded = () => (
+    <div style={{textAlign: "center"}}>
+      {dogs.map((dog) => (
+        <article key={dog._id}>
+          <img src ={dog.img} alt="puppy"/>
+          <h1>{dog.name}</h1>
+          <h3>{dog.age}</h3>
+          <button onClick={() => {
+            props.selectDog(dog);
+            props.history.push("/edit")
+          }}>
+            Edit
+          </button>
+          <button onClick={() => {
+            props.deleteDog(dog)
+          }}>
+            Delete
+          </button>                                                      
+        </article>
+      ))}
+    </div>
+  )
+
+  const loading = () => <h1>Loading...</h1>
+
+  return dogs.length > 0 ? loaded() : loading();
 };
 
 export default Display;
